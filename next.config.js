a64845bae935bfca2afa718b9885b0fdf750ae4e
Himangshu Mondal <himangshumondal.swadesh@gmail.com>
2022-08-03 19:17:12 +0530

    build: added sass and configure next config with sass, configure tsconfig for path imports

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "colors.scss"; @import "breakpoints.scss";`,
+  },
 };
 
 module.exports = nextConfig;
